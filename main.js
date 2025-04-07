f43478966391c65513551265c55b08d6b8781826
nialvo <nialvo@protonmail.com>
2025-04-06 23:47:44 -0500

    hell yeah

--- main.js
+++ main.js
@@ -233,8 +233,8 @@ window.onload = () => {
       if (alp < -180) alp += 360;
       else if (alp > 180) alp -= 360;
 
-      const gamma = THREE.MathUtils.degToRad(mobileRotation.gamma - calibration.gamma);        // left-right tilt
-      const beta   = THREE.MathUtils.degToRad(mobileRotation.beta );   // forward tilt
+      const gamma = THREE.MathUtils.degToRad(mobileRotation.gamma);// - calibration.gamma);       
+      const beta   = THREE.MathUtils.degToRad(mobileRotation.beta );  
       const alpha  = THREE.MathUtils.degToRad(alp);
 
       if (Math.abs(gamma) > 0.05) ship.quaternion.multiply(q.setFromAxisAngle(new THREE.Vector3(1, 0, 0), -gamma * 0.05));
