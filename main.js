cdc119e714ac2debdcc34bcea401f351f86190f7
nialvo <nialvo@protonmail.com>
2025-04-07 00:12:04 -0500

    hell yeah

--- main.js
+++ main.js
@@ -237,7 +237,7 @@ window.onload = () => {
       const beta   = THREE.MathUtils.degToRad(mobileRotation.beta );  
       const alpha  = THREE.MathUtils.degToRad(alp);
 
-      if (Math.abs(gamma) > 0.05) ship.quaternion.multiply(q.setFromAxisAngle(new THREE.Vector3(1, 0, 0), -gamma * 0.05));
+      if (Math.abs(gamma) > 0.05) ship.quaternion.multiply(q.setFromAxisAngle(new THREE.Vector3(0, 0, 1), -gamma * 0.05));
       if (Math.abs(alpha) > 0.05)   ship.quaternion.multiply(q.setFromAxisAngle(new THREE.Vector3(0, 1, 0), -alpha * 0.05));
       //if (Math.abs(beta) > 0.05)  ship.quaternion.multiply(q.setFromAxisAngle(new THREE.Vector3(0, 0, 1), beta * 0.001));
 
